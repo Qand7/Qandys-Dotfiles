3828b227e194b793ee9d6725462e3881e24828a4
Qandy <145124536+Qand7@users.noreply.github.com>
2026-06-14 20:57:20 +0200

    Create init.lua

---
+++ nvim/init.lua
@@ -0,0 +1,16 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.opt.autoindent = true
+
+vim.opt.background = "dark"
+
+vim.opt.spell = true
+
+vim.opt.spelllang = en_uk,de,es
+
+vim.opt.spellsuggest = "best"
