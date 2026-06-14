db10153ef7dff96932d07769ea9a3aaaf11ed1ae
Qandy <145124536+Qand7@users.noreply.github.com>
2026-06-14 16:21:46 +0200

    Add files via upload

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -1,1 +1,60 @@
+------------------
+---- MONITORS ----
+------------------
+require("modules/monitors")
 
+---------------------
+---- MY PROGRAMS ----
+---------------------
+require("modules/programs")
+
+-------------------
+---- AUTOSTART ----
+-------------------
+require("modules/autostart")
+
+-------------------------------
+---- ENVIRONMENT VARIABLES ----
+-------------------------------
+require("modules/env")
+
+---------------------
+---- DECORATIONS ----
+---------------------
+require("modules/decorations")
+
+--------------------
+---- ANIMATIONS ----
+--------------------
+require("modules/animations")
+
+-----------------
+---- LAYOUTS ----
+-----------------
+require("modules/layouts")
+
+----------------
+----  MISC  ----
+----------------
+require("modules/misc")
+
+---------------
+---- INPUT ----
+---------------
+require("modules/input")
+
+---------------------
+---- KEYBINDINGS ----
+---------------------
+require("modules/keybinds")
+
+--------------------------------
+---- WINDOWS AND WORKSPACES ----
+--------------------------------
+require("modules/windowrules")
+
+-----------------
+---- PLUGINS ----
+-----------------
+
+require("modules/plugins")
