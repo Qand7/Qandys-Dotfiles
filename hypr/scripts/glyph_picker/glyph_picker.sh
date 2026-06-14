5aa7000ec5bf431f5399dcaf766b7324a19d9a57
Qandy <145124536+Qand7@users.noreply.github.com>
2026-06-14 16:24:08 +0200

    Add files via upload

--- hypr/scripts/glyph_picker/glyph_picker.sh
+++ hypr/scripts/glyph_picker/glyph_picker.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+
+picker="wofi"
+
+if pgrep -x $picker > /dev/null; then
+	killall $picker
+else
+	cat "$HOME/.config/hypr/scripts/glyph_picker/nerdfont.txt" | wofi --dmenu  | awk '{print $1}' | wl-copy &
+fi
+
 
